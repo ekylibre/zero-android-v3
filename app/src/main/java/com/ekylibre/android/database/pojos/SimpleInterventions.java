3e3c3536fcda772fd5feb92d7b389fceed7823c5
Rémi de Chazelles <rdechazelles@ekylibre.com>
2018-07-24 10:57:29 +0200

    Finalizes query interventions by crop and production

---
+++ app/src/main/java/com/ekylibre/android/database/pojos/SimpleInterventions.java
@@ -0,0 +1,24 @@
+package com.ekylibre.android.database.pojos;
+
+import android.arch.persistence.room.Embedded;
+import android.arch.persistence.room.Relation;
+
+import com.ekylibre.android.database.models.Crop;
+import com.ekylibre.android.database.models.Intervention;
+import com.ekylibre.android.database.relations.InterventionCrop;
+import com.ekylibre.android.database.relations.InterventionWorkingDay;
+
+import java.util.List;
+
+public class SimpleInterventions {
+
+    @Embedded
+    public Intervention intervention;
+
+    @Relation(parentColumn = Intervention.COLUMN_ID, entityColumn = InterventionWorkingDay.COLUMN_INTERVENTION_ID, entity = InterventionWorkingDay.class)
+    public List<InterventionWorkingDay> workingDays;
+
+    @Relation(parentColumn = Intervention.COLUMN_ID, entityColumn = InterventionCrop.COLUMN_INTERVENTION_ID, entity = InterventionCrop.class)
+    public List<Crops> crops;
+
+}
