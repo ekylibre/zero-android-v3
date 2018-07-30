c8047ca6d6111ea097dab3be93e570fa4807822c
Rémi de Chazelles <remidechazelles@gmail.com>
2018-07-30 14:35:15 +0200

    Updates GraphQL schema to latest API changes

--- app/src/main/java/com/ekylibre/android/utils/Converters.java
+++ app/src/main/java/com/ekylibre/android/utils/Converters.java
@@ -1,4 +1,28 @@
 package com.ekylibre.android.utils;
 
+import android.arch.persistence.room.TypeConverter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class Converters {
+
+    private static final SimpleDateFormat ISO8601 = new SimpleDateFormat( "yyyy-MM-dd");
+
+    @TypeConverter
+    public static Date toDate(String value) {
+        try {
+            return value == null ? null : ISO8601.parse(value);
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(value + " is not a valid ISO 8601 date", e );
+        }
+    }
+
+    @TypeConverter
+    public static String toString(Date date) {
+        return date == null ? null : ISO8601.format(date);
+    }
+
 }
