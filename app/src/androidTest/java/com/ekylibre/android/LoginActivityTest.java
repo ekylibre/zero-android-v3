88c4341aca43a6cf5a97baef4bcb5e25d1646b02
Rémi de Chazelles <remidechazelles@gmail.com>
2018-11-22 14:14:33 +0100

    LoginActivity test with Espresso

---
+++ app/src/androidTest/java/com/ekylibre/android/LoginActivityTest.java
@@ -0,0 +1,153 @@
+package com.ekylibre.android;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import static android.content.Context.MODE_PRIVATE;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.junit.Assert.assertTrue;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class) {
+        @Override
+        protected void beforeActivityLaunched() {
+            clearSharedPrefs(ApplicationProvider.getApplicationContext());
+            super.beforeActivityLaunched();
+        }
+    };
+
+    private void clearSharedPrefs(Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        if (prefs.contains("is_authenticated")) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("is_authenticated", false);
+            editor.commit();
+        }
+    }
+
+    @Test
+    public void loginActivityTest() {
+        ViewInteraction textInputEditText = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.login_email),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText("rdechazelles@ekylibre.com"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText2 = onView(
+                allOf(withText("rdechazelles@ekylibre.com"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.login_email),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText2.perform(pressImeActionButton());
+
+        ViewInteraction textInputEditText3 = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.login_password),
+                                0),
+                        0),
+                        isDisplayed()));
+        textInputEditText3.perform(replaceText("123azeqsd"), closeSoftKeyboard());
+
+        ViewInteraction textInputEditText4 = onView(
+                allOf(withText("123azeqsd"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.login_password),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText4.perform(pressImeActionButton());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.sign_in_button), withText("Connexion"),
+                        childAtPosition(
+                                allOf(withId(R.id.login_layout),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                5),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+//        ViewInteraction textView = onView(
+//                allOf(withText("GAEC du bois joli"),
+//                        childAtPosition(
+//                                allOf(withId(R.id.action_bar),
+//                                        childAtPosition(
+//                                                withId(R.id.action_bar_container),
+//                                                0)),
+//                                0),
+//                        isDisplayed()));
+//        textView.check(matches(withText("GAEC du bois joli")));
+
+        Context context = mActivityTestRule.getActivity();
+        SharedPreferences prefs = context.getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean is_authenticated = prefs.getBoolean("is_authenticated", false);
+
+        assertTrue(is_authenticated);
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
